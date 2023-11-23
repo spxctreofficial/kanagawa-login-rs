04edecfa4a1829c8e95694589cab862299cbf6f9
spxctreofficial <spxctre@spxct.cf>
2023-11-23 14:03:38 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# kanagawa-login-rs
+
+A very simple but reactive login page built with Rust on the blazingly-fast Leptos framework.
+
+## Run
+
+Install the Rust Nightly toolchain with `rustup`:
+
+`rustup toolchain install nightly`
+
+Add `wasm` to your build targets:
+
+`rustup target add wasm32-unknown-unknown`
+
+Ensure that `trunk` and `cargo-leptos` is installed:
+
+`cargo install --locked trunk` (use the `--locked` flag to ensure no errors)
+
+`cargo install cargo-leptos`
+
+Clone the repository, `cd` into the directory, then run:
+
+`cargo leptos watch`
+
+Enjoy nerd
---
+++ src/app.rs
@@ -0,0 +1,78 @@
+use leptos::*;
+use leptos_meta::*;
+use leptos_router::*;
+
+#[component]
+pub fn App() -> impl IntoView {
+    // Provides context that manages stylesheets, titles, meta tags, etc.
+    provide_meta_context();
+
+    view! {
+        <Stylesheet id="leptos" href="/pkg/leptos_start.css"/>
+
+        // sets the document title
+        <Title text="login-rs"/>
+
+        // content for this welcome page
+        <Router>
+            <main>
+                <Routes>
+                    <Route path="" view=HomePage/>
+                    <Route path="/*any" view=NotFound/>
+                </Routes>
+            </main>
+        </Router>
+    }
+}
+
+/// Renders the home page of your application.
+#[component]
+fn HomePage() -> impl IntoView {
+    // Creates a reactive value to update the button
+    let (count, set_count) = create_signal(0);
+    let on_click = move |_| set_count.update(|count| *count += 1);
+
+    view! {
+        <html>
+            <body id="screen">
+                <div class="form-submit-panel">
+                    <section class="input-login-section">
+                        <div class="div-login" id="div-username">
+                            <label class="label-login">"Username:"</label>
+                            <input class="input-login" id="username"/>
+                        </div>
+                        <div class="div-login" id="div-password">
+                            <label class="label-login">"Password:"</label>
+                            <input class="input-login" id="username"/>
+                        </div>
+                    </section>
+                    <section class="form-submit-region">
+                        <button class="form-submit-btn" id="login-btn" on:click=on_click>
+                            {count}
+                        </button>
+                    </section>
+                </div>
+            </body>
+        </html>
+    }
+}
+
+/// 404 - Not Found
+#[component]
+fn not_found() -> impl IntoView {
+    // set an HTTP status code 404
+    // this is feature gated because it can only be done during
+    // initial server-side rendering
+    // if you navigate to the 404 page subsequently, the status
+    // code will not be set because there is not a new HTTP request
+    // to the server
+    #[cfg(feature = "ssr")]
+    {
+        // this can be done inline because it's synchronous
+        // if it were async, we'd use a server function
+        let resp = expect_context::<leptos_actix::ResponseOptions>();
+        resp.set_status(actix_web::http::StatusCode::NOT_FOUND);
+    }
+
+    view! { <h1>"Not Found"</h1> }
+}
